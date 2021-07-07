f642b8ddd52668a9e8d6fac25f542fc17e5cbecc
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-07-08 01:12:06 +0200

    Extend the KMIP spec 1.0 use case 8.1 with the Create Key Pair response.

--- src/tests/kmip_10_use_case_8_1_create_key_pair.rs
+++ src/tests/kmip_10_use_case_8_1_create_key_pair.rs
@@ -2,19 +2,23 @@
 
 use krill_kmip_ttlv::ser::to_vec;
 
-use crate::types::{
-    common::{
-        Attribute, CommonTemplateAttribute, CryptographicAlgorithm, CryptographicUsageMask, Operation,
-        PrivateKeyTemplateAttribute, PublicKeyTemplateAttribute,
-    },
-    request::{
-        self, Authentication, BatchCount, BatchItem, MaximumResponseSize, ProtocolVersionMajor, ProtocolVersionMinor,
-        RequestHeader, RequestMessage, RequestPayload,
+use crate::{
+    response::from_slice,
+    types::{
+        common::{
+            Attribute, CommonTemplateAttribute, CryptographicAlgorithm, CryptographicUsageMask, Operation,
+            PrivateKeyTemplateAttribute, PublicKeyTemplateAttribute,
+        },
+        request::{
+            self, Authentication, BatchCount, BatchItem, MaximumResponseSize, ProtocolVersionMajor,
+            ProtocolVersionMinor, RequestHeader, RequestMessage, RequestPayload,
+        },
+        response::{ResponseMessage, ResponsePayload, ResultStatus},
     },
 };
 
 #[test]
-fn create_key_pair_rsa_1024() {
+fn create_key_pair_request_rsa_1024() {
     let use_case_request = RequestMessage(
         RequestHeader(
             request::ProtocolVersion(ProtocolVersionMajor(1), ProtocolVersionMinor(0)),
@@ -57,4 +61,40 @@ fn create_key_pair_rsa_1024() {
     let actual_request_hex = hex::encode_upper(to_vec(&use_case_request).unwrap());
 
     assert_eq!(use_case_request_hex, actual_request_hex);
+}
+
+#[test]
+fn create_key_pair_response() {
+    let use_case_response_hex = concat!(
+        "42007B01000000E042007A0100000048420069010000002042006A0200000004000000010000000042006B02000000040",
+        "0000000000000004200920900000008000000004B73C13A42000D0200000004000000010000000042000F010000008842",
+        "005C0500000004000000020000000042007F0500000004000000000000000042007C01000000604200940700000024383",
+        "93566373263322D623230612D343964382D393530342D3664633231313563633034320000000042009407000000246132",
+        "3432666361342D656266302D343339382D616336352D38373962616234393032353900000000"
+    );
+    let ttlv_wire = hex::decode(use_case_response_hex).unwrap();
+    let res: ResponseMessage = from_slice(ttlv_wire.as_ref()).unwrap();
+
+    assert_eq!(res.header.protocol_version.major, 1);
+    assert_eq!(res.header.protocol_version.minor, 0);
+    assert_eq!(res.header.timestamp, 0x000000004B73C13A);
+    assert_eq!(res.header.batch_count, 1);
+    assert_eq!(res.batch_items.len(), 1);
+
+    let item = &res.batch_items[0];
+    assert!(matches!(item.result_status, ResultStatus::Success));
+    assert!(matches!(&item.payload, Some(ResponsePayload::CreateKeyPair(_))));
+
+    if let Some(ResponsePayload::CreateKeyPair(payload)) = item.payload.as_ref() {
+        assert_eq!(
+            &payload.private_key_unique_identifier,
+            "895f72c2-b20a-49d8-9504-6dc2115cc042"
+        );
+        assert_eq!(
+            &payload.public_key_unique_identifier,
+            "a242fca4-ebf0-4398-ac65-879bab490259"
+        );
+    } else {
+        panic!("Wrong payload");
+    }
 }
