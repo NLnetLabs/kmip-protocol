d5ad777c7200fb91480f5b1f9812dba1f2e2c891
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-07-13 11:59:49 +0200

    Extend the KMIP spec 1.1 test case 4.1 with the client b get attributes request and response.

--- src/tests/kmip_10_use_case_4_1_key_lifecycle.rs
+++ src/tests/kmip_10_use_case_4_1_key_lifecycle.rs
@@ -177,10 +177,9 @@ fn client_a_get_state_attribute_response() {
         assert!(payload.attributes.is_some());
         if let Some(attributes) = &payload.attributes {
             assert_eq!(attributes.len(), 1);
-
-            let attr = &attributes[0];
-            assert_eq!(&attr.name, "State");
-            assert!(matches!(attr.value, AttributeValue::State(State::PreActive)));
+            assert_eq!(attributes.len(), 1);
+            assert_eq!(&attributes[0].name, "State");
+            assert!(matches!(attributes[0].value, AttributeValue::State(State::PreActive)));
         }
     } else {
         panic!("Wrong payload");
@@ -327,10 +326,9 @@ fn client_a_get_state_attribute_response2() {
         assert!(payload.attributes.is_some());
         if let Some(attributes) = &payload.attributes {
             assert_eq!(attributes.len(), 1);
-
-            let attr = &attributes[0];
-            assert_eq!(&attr.name, "State");
-            assert!(matches!(attr.value, AttributeValue::State(State::Active)));
+            assert_eq!(attributes.len(), 1);
+            assert_eq!(&attributes[0].name, "State");
+            assert!(matches!(attributes[0].value, AttributeValue::State(State::Active)));
         }
     } else {
         panic!("Wrong payload");
@@ -571,6 +569,86 @@ fn client_b_revoke_response_symmetric_key_compromised() {
 
     if let Some(ResponsePayload::Revoke(payload)) = item.payload.as_ref() {
         assert_eq!(&payload.unique_identifier, KEY_ID);
+    } else {
+        panic!("Wrong payload");
+    }
+}
+
+#[test]
+fn client_b_get_state_attribute_request() {
+    let use_case_request = RequestMessage(
+        RequestHeader(
+            request::ProtocolVersion(ProtocolVersionMajor(1), ProtocolVersionMinor(0)),
+            Option::<MaximumResponseSize>::None,
+            Option::<Authentication>::None,
+            BatchCount(1),
+        ),
+        vec![BatchItem(
+            Operation::GetAttributes,
+            RequestPayload::GetAttributes(
+                Some(UniqueIdentifier(KEY_ID.into())),
+                Some(vec![AttributeName("State".into())]),
+            ),
+        )],
+    );
+
+    let use_case_request_hex = concat!(
+        "42007801000000A04200770100000038420069010000002042006A0200000004000000010000000042006B02000000040",
+        "00000000000000042000D0200000004000000010000000042000F010000005842005C05000000040000000B0000000042",
+        "00790100000040420094070000002432316432386238612D303664662D343363302D623732662D3261313631363333616",
+        "461390000000042000A07000000055374617465000000",
+    );
+    let actual_request_hex = hex::encode_upper(to_vec(&use_case_request).unwrap());
+
+    assert_eq!(use_case_request_hex, actual_request_hex);
+}
+
+#[test]
+fn client_b_get_state_attribute_response() {
+    // From: https://docs.oasis-open.org/kmip/usecases/v1.0/cs01/kmip-usecases-1.0-cs-01.html#_Toc262822060
+    // Tag: Response Message (0x42007B), Type: Structure (0x01), Data:
+    //   Tag: Response Header (0x42007A), Type: Structure (0x01), Data:
+    //     Tag: Protocol Version (0x420069), Type: Structure (0x01), Data:
+    //       Tag: Protocol Version Major (0x42006A), Type: Integer (0x02), Data: 0x00000001 (1)
+    //       Tag: Protocol Version Minor (0x42006B), Type: Integer (0x02), Data: 0x00000000 (0)
+    //     Tag: Time Stamp (0x420092), Type: Date-Time (0x09), Data: 0x000000004AFBED2C (Thu Nov 12 12:10:36 CET 2009)
+    //     Tag: Batch Count (0x42000D), Type: Integer (0x02), Data: 0x00000001 (1)
+    //   Tag: Batch Item (0x42000F), Type: Structure (0x01), Data:
+    //     Tag: Operation (0x42005C), Type: Enumeration (0x05), Data: 0x0000000B (Get Attributes)
+    //     Tag: Result Status (0x42007F), Type: Enumeration (0x05), Data: 0x00000000 (Success)
+    //     Tag: Response Payload (0x42007C), Type: Structure (0x01), Data:
+    //       Tag: Unique Identifier (0x420094), Type: Text String (0x07), Data: 21d28b8a-06df-43c0-b72f-2a161633ada9
+    //       Tag: Attribute (0x420008), Type: Structure (0x01), Data:
+    //         Tag: Attribute Name (0x42000A), Type: Text String (0x07), Data: State
+    //         Tag: Attribute Value (0x42000B), Type: Enumeration (0x05), Data: 0x00000004 (Compromised)
+    let use_case_response_hex = concat!(
+        "42007B01000000D842007A0100000048420069010000002042006A0200000004000000010000000042006B02000000040",
+        "0000000000000004200920900000008000000004AFBED2C42000D0200000004000000010000000042000F010000008042",
+        "005C05000000040000000B0000000042007F0500000004000000000000000042007C01000000584200940700000024323",
+        "16432386238612D303664662D343363302D623732662D3261313631363333616461390000000042000801000000204200",
+        "0A0700000005537461746500000042000B05000000040000000400000000",
+    );
+    let ttlv_wire = hex::decode(use_case_response_hex).unwrap();
+    let res: ResponseMessage = from_slice(ttlv_wire.as_ref()).unwrap();
+
+    assert_eq!(res.header.protocol_version.major, 1);
+    assert_eq!(res.header.protocol_version.minor, 0);
+    assert_eq!(res.header.timestamp, 0x000000004AFBED2C);
+    assert_eq!(res.header.batch_count, 1);
+    assert_eq!(res.batch_items.len(), 1);
+
+    let item = &res.batch_items[0];
+    assert!(matches!(item.result_status, ResultStatus::Success));
+    assert!(matches!(item.operation, Some(Operation::GetAttributes)));
+    assert!(matches!(&item.payload, Some(ResponsePayload::GetAttributes(_))));
+
+    if let Some(ResponsePayload::GetAttributes(payload)) = item.payload.as_ref() {
+        assert!(payload.attributes.is_some());
+        if let Some(attributes) = &payload.attributes {
+            assert_eq!(attributes.len(), 1);
+            assert_eq!(&attributes[0].name, "State");
+            assert!(matches!(attributes[0].value, AttributeValue::State(State::Compromised)));
+        }
     } else {
         panic!("Wrong payload");
     }
