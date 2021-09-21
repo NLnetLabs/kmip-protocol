b27eddae94ddf56ec8b1fc2f7878dbeea3ae8865
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-09-21 12:15:02 +0200

    cargo fmt.

--- examples/demo/main.rs
+++ examples/demo/main.rs
@@ -1,4 +1,10 @@
-#[cfg(not(any(feature = "tls-with-openssl", feature = "tls-with-rustls", feature = "tls-with-tokio-native", feature = "tls-with-tokio-rustls", feature = "tls-with-async-tls")))]
+#[cfg(not(any(
+    feature = "tls-with-openssl",
+    feature = "tls-with-rustls",
+    feature = "tls-with-tokio-native",
+    feature = "tls-with-tokio-rustls",
+    feature = "tls-with-async-tls"
+)))]
 compile_error!("This demo requires one of the tls-with-xxx features to be enabled.");
 
 mod config;
