8a760cfbda4766f890c77513847636b5d0011026
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-10-05 12:41:02 +0200

    Bump version number reference in docs.

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! kmip-protocol = { version = "0.3.0", features = ["tls-with-rustls"] }
+//! kmip-protocol = { version = "0.3.1", features = ["tls-with-rustls"] }
 //! ```
 //!
 //! [Rustls]: https://github.com/rustls/rustls
