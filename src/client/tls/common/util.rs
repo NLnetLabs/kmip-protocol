417dcfa54ad5c846b9d7b15dd1388acddf3688ce
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-10-05 16:52:55 +0200

    FIX: In the example demo don't deactivate request/response capturing and logging if no max response bytes size is set.

--- src/client/tls/common/util.rs
+++ src/client/tls/common/util.rs
@@ -9,10 +9,12 @@ pub(crate) fn create_kmip_client<T>(tls_stream: T, conn_settings: &ConnectionSet
         client = client.with_credentials(username.clone(), conn_settings.password.clone());
     }
 
+    let mut reader_config = KmipConfig::default().with_read_buf();
     if let Some(max_bytes) = conn_settings.max_response_bytes {
-        let reader_config = KmipConfig::default().with_max_bytes(max_bytes).with_read_buf();
-        client = client.with_reader_config(reader_config);
+        reader_config = reader_config.with_max_bytes(max_bytes);
     };
+
+    client = client.with_reader_config(reader_config);
 
     client.build()
 }
