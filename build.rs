0161f22e0422acd01d6a88d22bd931c2bef77e23
ximon18 <3304436+ximon18@users.noreply.github.com>
2022-10-18 12:58:19 +0200

    Permit building with Rust 1.49.0, we only need newer 1.54.0 for the tests.

--- build.rs
+++ build.rs
@@ -3,9 +3,9 @@ use rustc_version::{version, Version};
 
 fn main() {
     let version = version().expect("Failed to get rustc version.");
-    if version < Version::parse("1.54.0").unwrap() {
+    if version < Version::parse("1.49.0").unwrap() {
         eprintln!(
-            "\n\nAt least Rust version 1.54 is required.\n\
+            "\n\nAt least Rust version 1.49 is required.\n\
              Version {} is used for building.\n\
              Build aborted.\n\n",
             version
